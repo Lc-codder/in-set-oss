90135977191d41e79ebd2af0a6f458ebe7f7efc6
Lucas Daniel <guimaraesslucas@gmail.com>
2026-05-10 11:16:43 -0300

    estrutura do projeto e BancoDeDados

--- in-set-oss/core/settings.py
+++ in-set-oss/core/settings.py
@@ -61,15 +61,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'insetoss',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'OPTIONS': {
-            'client_encoding': 'UTF8',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
